073808444307a6e258dd38036d0353052f70322f
Aheebwa Ramadhan <raaheebwa@gmail.com>
2022-09-06 01:27:50 +0300

    chore: update env file and main with port

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const PORT = process.env.APP_PORT || 3001;
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(PORT);
+  Logger.log(`Server running on http://localhost:${PORT}`, 'Bootstrap');
 }
 bootstrap();
