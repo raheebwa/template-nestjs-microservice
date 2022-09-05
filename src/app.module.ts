9b6f58abdb34df41a62ce457095e0b720029d902
Aheebwa Ramadhan <raaheebwa@gmail.com>
2022-09-06 01:52:00 +0300

    chore: can work with gql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
+
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: 'src/schema.graphql',
+      plugins: [ApolloServerPluginLandingPageLocalDefault],
+      playground: false,
+    }),
     ConfigModule.forRoot({
       isGlobal: true,
     }),
